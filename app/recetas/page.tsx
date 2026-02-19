73d66c592b91388441fe3049af0f1e10a3552e25
Worome <ellaboratoriotecnologico@gmail.com>
2026-02-19 08:32:41 +0100

    Primer commit: proyecto Next.js Cocina Inteligente

---
+++ app/recetas/page.tsx
@@ -0,0 +1,9 @@
+// src/app/recetas/page.tsx
+export default function Home() {
+  return (
+    <main className="p-10">
+      <h1 className="text-3xl font-bold">Cocina Inteligente 🍳</h1>
+      <p className="mt-4">Planifica tus recetas y optimiza tu gasto.</p>
+    </main>
+  );
+}
